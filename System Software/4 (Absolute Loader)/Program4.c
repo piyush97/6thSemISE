a16d8122d4373671fbb736e3b878b84e9979d8f3
Piyush Mehta <me@piyushmehta.com>
2019-04-21 15:26:10 +0530

    Program 4 solved

--- System Software/4 (Absolute Loader)/Program4.c
+++ System Software/4 (Absolute Loader)/Program4.c
@@ -4,23 +4,23 @@
 
 void main()
 {
-	FILE *input;
-	input = fopen("input.txt", "r");
+	FILE *inputFile;
+	inputFile = fopen("input.txt", "r");
 	char type, objcode[20], progname[20], input[20];
 	int sa, addr, len;
-	fscanf(input, "%c %s %x %x", &type, progname, &sa, &len);
+	fscanf(inputFile, "%c %s %x %x", &type, progname, &sa, &len);
 
 	printf("Loading Program %s of length %06X starting at Address %06X :-\n", progname, len, sa);
 
-	fscanf(input, "%s", input);
+	fscanf(inputFile, "%s", input);
 
 	while (strcmp(input, "E") != 0)
 	{
 		if (strcmp(input, "T") == 0)
 		{
-			fscanf(input, "%X", &addr);
-			fscanf(input, "%X", &len);
-			fscanf(input, "%s", input);
+			fscanf(inputFile, "%X", &addr);
+			fscanf(inputFile, "%X", &len);
+			fscanf(inputFile, "%s", input);
 		}
 
 		int i = 0;
@@ -30,7 +30,7 @@ void main()
 			addr++;
 			i += 2;
 		}
-		fscanf(input, "%s", input);
+		fscanf(inputFile, "%s", input);
 	}
-	fclose(input);
+	fclose(inputFile);
 }
