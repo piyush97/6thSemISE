58ae9ebd26ee08748c710949df513285353b8d40
Piyush Mehta <me@piyushmehta.com>
2019-05-15 21:14:58 +0530

    Abstract Factory

---
+++ Design Patterns with Java/Abstract-Factory/Factory.java
@@ -0,0 +1,15 @@
+
+public abstract class Factory {
+
+    public Factory() {
+    	System.out.println("factory instantiated!");
+    }
+
+	public outdoorAdventure categoryA(String ptype) {
+		return null;
+	}
+	public outdoorRegular categoryR(String ptype) {
+		return null;
+	}
+	
+}
